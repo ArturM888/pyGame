8114eb24e58ed41896f285e54924fb21f640b827
ArturM888 <artur.moczybroda@gmail.com>
2020-08-20 16:35:27 +0200

    Dodane poruszanie się piłki, funkcje sprawdzające czy piłka przekroczyła rakietkę i gracz zdobywa punkt, funkcja sprawdzająca czy piłka ma się odbić od rakietki oraz zliczanie punktów i wyświetlenie punktów graczy w okienku gry.

--- pong.py
+++ pong.py
@@ -6,6 +6,11 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
 
+# Punkty
+score_a = 0
+score_b = 0
+
+
 # Rakietka gracza A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -34,6 +39,19 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
+ball.dx = 0.1
+ball.dy = -0.1
+
+
+# Wyświetlanie punktacji
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 
 # Funkcje
 def paddle_a_up():
@@ -73,3 +91,40 @@ running = True
 
 while running:
     window.update()
+
+    # Poruszanie piłki
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+
+    # Sprawdzenie krawędzi okienka
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b +=1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
+
+    # Sprawdzenie czy piłka odbija się od rakietki
+    if (ball.xcor() > 340 and ball.xcor() < 350 ) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350 ) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
